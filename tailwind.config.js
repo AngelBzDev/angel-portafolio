5841fcc8992b86c5c8ed8f3b453cc88371b4a4f7
AngelBzDev <angelbaez.dev@gmail.com>
2022-06-05 15:18:58 -0500

    Config🔧: TailwindCss

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif']
+    },
+    colors: {
+      //p- significa personalizado
+      'p-blue': '#161622',
+      'p-white': '#eff0f2',
+      'p-blue-2': '#c5f1fe'
+    },
+    extend: {},
+  },
+  plugins: [],
+};
