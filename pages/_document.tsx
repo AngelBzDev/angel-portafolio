7813f29b4c831e7a1ce86286fef2888723439e9e
AngelBzDev <angelbaez.dev@gmail.com>
2022-06-05 15:21:07 -0500

    Config🔧: Idioma y cambio de color de fondo

---
+++ pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang='es'>
+      <Head />
+      <body className='bg-p-blue text-p-white'>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
