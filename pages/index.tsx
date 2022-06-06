64c01b78f9a1d5d408c9be97f4f317bc62501fde
AngelBzDev <angelbaez.dev@gmail.com>
2022-06-05 21:48:35 -0500

    Add🤠: Info principal

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,21 @@ const Home: NextPage = () => {
   return (
     <MainLayout>
       <main className="px-36">
-
+        <section className="flex flex-col items-start justify-center max-w-5xl mx-auto screen">
+          <div>
+            <p className="mb-8 text-base text-p-purple">Hola, mi nombre es</p>
+            <h1 className="font-extrabold text-7xl">Ángel Baez</h1>
+            <h2 className="mt-5 text-6xl font-bold capitalize text-p-blue-2">
+              Y soy desarrollador web
+            </h2>
+            <p className="mt-6 text-xl">
+              Soy un Ingeniro en Desarrollo y Gestión de software
+            </p>
+          </div>
+        </section>
       </main>
     </MainLayout>
-  )
+  );
 };
 
 export default Home;
